d5e3edcb7ae3e6e6310c92758c1340925f9829b3
Mariana Merlim Rodrigues <104798332+Marimerlim@users.noreply.github.com>
2022-07-15 14:16:20 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Prática React 02 - Component de Class
+
+// Crie um Component de class que retorne
+//uma frase na tela
+
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <h1>Uma frase na tela</h1>;
+  }
+}
+
+export default App;
